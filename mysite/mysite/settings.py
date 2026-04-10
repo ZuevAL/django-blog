ff5a4ecbc635b348c674fb5130a828af6b55307b
Egor <zueve2007@gmail.com>
2026-04-10 15:24:56 +0300

    made sqlite3 the main database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'blog',
-        'PASSWORD': '12345',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
